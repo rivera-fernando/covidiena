0c8cb5e6b3c10127b3f44b36cd557cf6c17b9a7e
Thomas Cintra <tcintra@g.hmc.edu>
2020-06-30 21:39:03 +0000

    Create a cron job to handle covid data. Cron will update covidData entities in datastore every hour by scraping wikipedia. Web scraping now uses HTMLUnit in java servlet instead of client-side on javascript which is faster and avoids any runtime errors when trying to scrape the page.

---
+++ project/src/main/java/com/google/sps/servlets/LoadCovidDataServlet.java
@@ -0,0 +1,69 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.gson.Gson;
+import com.google.sps.classes.CountryData;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.sps.data.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+/** Servlet that loads pending events*/
+@WebServlet("/load-covid-data")
+public class LoadCovidDataServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Query query = new Query("CovidData");
+    query.addSort("cases", SortDirection.DESCENDING);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+    List<Entity> resultsList = results.asList(FetchOptions.Builder.withDefaults());
+    List<CountryData> covidDataList = new ArrayList<>();
+
+    for (int i = 0; i < resultsList.size(); i++) {
+      Entity entity = resultsList.get(i);
+      long id = entity.getKey().getId();
+      String location = (String) entity.getProperty("location");
+      long cases = (long) entity.getProperty("cases");
+      String deaths = (String) entity.getProperty("deaths");
+      String recoveries = (String) entity.getProperty("recoveries");
+
+      CountryData countryData = new CountryData(id, location, cases, deaths, recoveries);
+
+      covidDataList.add(countryData);
+    }
+
+    Gson gson = new Gson();
+
+    response.setContentType("application/json;");
+    response.getWriter().println(gson.toJson(covidDataList));
+  }
+}
