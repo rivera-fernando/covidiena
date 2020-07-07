20c688d7a3fe983e3abf56650cb580cf46de253f
Christina <christinachance315@gmail.com>
2020-07-07 18:52:44 +0000

    deleted unused code

--- project/src/main/java/com/google/sps/classes/EmailSender.java
+++ project/src/main/java/com/google/sps/classes/EmailSender.java
@@ -17,31 +17,6 @@ import javax.mail.Session;
 /*EmailSender creates and sends an email when users create and account to verify that they created the account*/
 public class EmailSender{ 
     public static void sendEmail(String to) {
-        // String from = "covidiena@gmail.com";
-        // String host = "127.0.0.1";
-        // Properties properties = System.getProperties();
-        // properties.setProperty("smtp.gmail.com", host);
-        // Session session = Session.getDefaultInstance(properties);
-
-        // try {
-        //     MimeMessage message = new MimeMessage(session);
-        //     message.setFrom(new InternetAddress(from));
-        //     message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-        //     message.setSubject("[Covidiena] Account Verification");
-        //     StringBuilder htmlBuilder = new StringBuilder();
-        //     htmlBuilder.append("<h1>");
-        //     htmlBuilder.append("An account on Covidiena has been created with this email. To verify this is you, please click");
-        //     htmlBuilder.append("<a href='covidiena.appspot.com/login.html'>here</a>");
-        //     htmlBuilder.append("</h1>");
-        //     String html = htmlBuilder.toString();
-        //     message.setContent(html, "text/html");
-        //     Transport.send(message);
-        //     System.out.println("Sent message successfully....");
-        // } catch (MessagingException mex) {
-        //     mex.printStackTrace();
-        // }
-
-
 
       // Put sender’s address
       String from = "covidiena@gmail.com";
